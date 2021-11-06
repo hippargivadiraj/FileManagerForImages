b3f14a0d9f9ac7eb3834283c5b5c2ec9a731bc2c
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-11-06 16:48:02 -0700

    Initial Commit

---
+++ FileManagerForImages/FileManagerForImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerForImagesApp.swift
+//  FileManagerForImages
+//
+//  Created by Vadiraj Hippargi on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerForImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
